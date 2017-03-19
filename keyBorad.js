11a895d7658b43d1285d94455dd382b9ff23dd71
xumengly <xumeng_ly@163.com>
2017-03-19 10:51:31 +0800

    “上传网站源码到github”

---
+++ keyBorad.js
@@ -0,0 +1,71 @@
+// JavaScript Document
+var KEY_LEFT = 0x25;
+var KEY_UP = 0x26;
+var KEY_RIGHT = 0x27;
+var KEY_DOWN = 0x28;
+var KEY_RETURN = 0x08; 	//回退键和返回键同样处理
+var KEY_OK = 0x0D; 			//确认;
+var KEY_PAGE_UP = 0x21;		//上页;
+var KEY_PAGE_DOWN = 0x22; //下页;
+var KEY_BACK = 0x18;
+var KEY_0 = 0x30;
+var KEY_1 = 0x31;
+var KEY_2 = 0x32;
+var KEY_3 = 0x33;
+var KEY_4 = 0x34;
+var KEY_5 = 0x35;
+var KEY_6 = 0x36;
+var KEY_7 = 0x37;
+var KEY_8 = 0x38;
+var KEY_9 = 0x39;
+var KEY_Pound = 0x69;
+var KEY_Star = 0x6A;
+	
+var KEY_A = 0x41; //华为海外遥控器彩色键
+var KEY_B = 0x42; //华为海外遥控器彩色键
+var KEY_C = 0x43; //华为海外遥控器彩色键
+var KEY_D = 0x44; //华为海外遥控器彩色键
+
+var KEY_EPG = 0x70;			//节目单
+var KEY_SET = 0x71;			//设置
+var KEY_POWER = 0x100;	//电源
+var KEY_ADD = 0x101;		//频道+
+var KEY_DEC = 0x102;		//频道-
+var KEY_VOLUME_ADD = 0x103;		//音量+
+var KEY_VOLUME_DEC = 0x104;		//音量-
+var KEY_MUTE = 0x105;			//静音
+var KEY_SOUNT_TRACK = 0x106;	//声道
+var KEY_PLAY = 0x107;			//播放/暂停
+var KEY_SPEED = 0x108;		//快进
+var KEY_REVERSE = 0x109;	//快退
+var KEY_GO_END = 0x10A;		//
+var KEY_GO_BEGIN = 0x10B;	//
+var KEY_INFO = 0x10C;			//
+var KEY_INTERACT = 0x10D;	//互动
+var KEY_STOP = 0x10E;			//停止
+var KEY_POSITION = 0x10F;	//定位
+var KEY_PORTAL = 0x110;		//首页
+
+var KEY_FAVORITE = 0x119;	//收藏
+var KEY_BOOKMARK = 0x11A;	//书签
+var KEY_SBL = 0x11B;			//-/--
+var KEY_HELP = 0x11C;			//帮助
+var KEY_SEARCH = 0x11D;		//搜索
+var KEY_NVOD = 0x11E;			//轮播
+
+var KEY_RED = 0x113;
+var KEY_GREEN = 0x114;
+var KEY_YELLOW = 0x115;
+var KEY_BLUE = 0x116;
+
+var KEY_TV_MENU = 0x113;		//文广红键 直播
+var KEY_TVOD_MENU = 0x114;	//文广绿键 回看
+var KEY_VOD_MENU = 0x115;		//文广黄键 点播
+var KEY_INFOMENU = 0x116;		//文广蓝键 信息
+var KEY_APPMENU = 0x117;		//文广灰色 应用
+
+var KEY_MENU_COMM = 0x451;	//搜索;
+var KEY_MENU_TV = 0x454; 		//频道;
+var KEY_MENU_VOD = 0x455; 	//点播;
+var KEY_MENU_TVOD = 0x456;	//回看;	
+var KEY_MENU_INFO = 0x458;	//信息;
